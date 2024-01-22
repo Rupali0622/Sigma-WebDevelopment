73610149963d0a62ed02448334dc5abaca9f55d7
Rupali <rupalichauhan022@gmail.com>
2024-01-22 22:22:08 +0530

    sigma

---
+++ one.java
@@ -0,0 +1,10 @@
+public class one{
+    public static void main(String[] args) {
+        int num=0b101;
+        int num2=0x7E;
+        int num3=10_00_00_000;
+        System.out.println(num);
+        System.out.println(num2);
+        System.out.println(num3);
+    }
+}
